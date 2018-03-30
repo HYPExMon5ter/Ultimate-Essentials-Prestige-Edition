9a3033bfff119bc04402ff6327ac48b8a576a909
HYPExMon5ter <BrackettBlake@gmail.com>
2018-03-30 17:30:00 -0400

    Added packages for commands

---
+++ src/net/hypexmon5ter/uepe/commands/Gamemode.java
@@ -0,0 +1,14 @@
+package net.hypexmon5ter.uepe.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Gamemode implements CommandExecutor {
+
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        
+        return true;
+    }
+}
